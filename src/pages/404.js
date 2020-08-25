4160b37dba55f9012299bd250fec724de34aa13a
thenabeelhassan <info@thenabeelhassan.com>
2020-08-26 01:43:54 +0500

    commit to master

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,15 @@ import React from 'react'
 export default function NotFound() {
 
     return (
-        () => {window.location = '/';}
+        <>
+            <h2>
+                PAGE NOT FOUND 
+            </h2>
+            <a
+                href='https://disguys.netlify.app'
+            >
+                DISGUY'S WARDOBE
+            </a>
+        </>
     );
 }
